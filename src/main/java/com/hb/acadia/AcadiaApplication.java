75af16b5aa4eed49b0855a20ca1dbc07a9af8c08
alalami63 <alalami@humanbooster.com>
2019-02-15 14:37:49 +0100

    Initialize project

---
+++ src/main/java/com/hb/acadia/AcadiaApplication.java
@@ -0,0 +1,14 @@
+package com.hb.acadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadiaApplication.class, args);
+	}
+
+}
+
