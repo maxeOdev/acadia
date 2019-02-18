26160e7f61bcc14762c7d1c5a29b7a2fa30ed051
alalami63 <alalami@humanbooster.com>
2019-02-18 14:40:32 +0100

    Test conflict again

--- src/main/java/com/hb/acadia/model/Address.java
+++ src/main/java/com/hb/acadia/model/Address.java
@@ -19,30 +19,7 @@ public class Address {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	@NotNull 
-	private int number;
-	@NotNull 
-	private String road;
-	@NotNull 
-	private String roadType;
-	@NotNull 
-	private String cp;
-	@NotNull 
-	private String city;
-	@NotNull 
-	private String country;
-	
-	
-	
-	@Override
-	public String toString() {
-		return "Adress [id=" + id + ", number=" + number + ", road=" + road + ", roadType=" + roadType + ", cp=" + cp
-				+ ", city=" + city + ", country=" + country + "]";
-	}
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	@NotNull 
-	private int number;
+	private int nomber;
 	@NotNull 
 	private String road;
 	@NotNull 
