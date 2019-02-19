c25331d103a1177dfa2b48b1aa0610be4d13b76b
Aliotti <sim.aliotti@gmail.com>
2019-02-19 16:32:36 +0100

    Fix conflict after merge on test

--- src/test/java/com/hb/acadia/service/CategoryServiceTest.java
+++ src/test/java/com/hb/acadia/service/CategoryServiceTest.java
@@ -1,14 +1,9 @@
 package com.hb.acadia.service;
 
-<<<<<<< HEAD
-=======
-import static org.assertj.core.api.Assertions.assertThat;
->>>>>>> 8964e946f697309e68672e8362170ca65c796276
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.junit.After;
@@ -41,20 +36,12 @@ public class CategoryServiceTest extends AbstractApplicationTest {
 		Category category2 = categoryService.createCategory(categoryName2);
 	}
 
-=======
-import org.junit.Test;
-import com.hb.acadia.model.Category;
-
-public class CategoryServiceTest extends AbstractApplicationTest {
-
->>>>>>> 8964e946f697309e68672e8362170ca65c796276
 	@Test
 	public void test_createCategory() {
 
 		String categoryName = "Java";
 		Category category = categoryService.createCategory(categoryName);
 
-<<<<<<< HEAD
 		String categoryName2 = "C++";
 		Category category2 = categoryService.createCategory(categoryName2);
 
@@ -79,23 +66,4 @@ public class CategoryServiceTest extends AbstractApplicationTest {
 		assertThat(categories.get(1).getId(), notNullValue());
 	}
 
-=======
-		// assertions
-		assertThat(category.getId(), notNullValue());
-		assertEquals(1, categoryRepository.count());
-		assertEquals(categoryName, category.getName());
-	}
-
-	@Test
-	public void test2_createCategory() {
-
-		String categoryName = "C++";
-		Category category = categoryService.createCategory(categoryName);
-
-		// assertions
-		assertThat(category.getId(), notNullValue());
-		assertEquals(2, categoryRepository.count());
-		assertEquals(categoryName, category.getName());
-	}
->>>>>>> 8964e946f697309e68672e8362170ca65c796276
 }
