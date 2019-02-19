35efb282e08c39af04d89c20e4f354926005c813
alalami63 <alalami@humanbooster.com>
2019-02-19 15:38:13 +0100

    New property file for local execution Fix UserMetadata table generation bug

--- src/main/java/com/hb/acadia/model/user/UserMetadata.java
+++ src/main/java/com/hb/acadia/model/user/UserMetadata.java
@@ -9,19 +9,16 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.hb.acadia.model.Video;
-import com.hb.acadia.repository.BillRepository;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @Builder
+@Entity
 @AssociationOverrides({
 		@AssociationOverride(name = "items.user", joinColumns = @JoinColumn(name = "idUser")),
 		@AssociationOverride(name = "items.training", joinColumns = @JoinColumn(name = "idTraining"))})
@@ -33,14 +30,14 @@ public class UserMetadata {
 	@Column(nullable = false, unique = true)
 	private String uuid;
 
-	private int currentTime;
+	private int currentTimeVideo;
 
 	@OneToOne(fetch = FetchType.LAZY)
 	private Video currentVideo;
 
 	@Override
 	public String toString() {
-		return "UserMetadata [items=" + items + ", uuid=" + uuid + ", currentTime=" + currentTime + ", currentVideo="
+		return "UserMetadata [items=" + items + ", uuid=" + uuid + ", currentTime=" + currentTimeVideo + ", currentVideo="
 				+ currentVideo + "]";
 	}
 
