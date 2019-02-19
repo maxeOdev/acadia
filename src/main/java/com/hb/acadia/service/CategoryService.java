6e2bf6f0d3fb92e362fbeb6b71733ae7e13f679f
Aliotti <sim.aliotti@gmail.com>
2019-02-19 11:48:02 +0100

    Add application properties file for tests

--- src/main/java/com/hb/acadia/service/CategoryService.java
+++ src/main/java/com/hb/acadia/service/CategoryService.java
@@ -1,8 +1,24 @@
 package com.hb.acadia.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hb.acadia.model.Category;
+import com.hb.acadia.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Transactional
+	public void createCategory(String categoryName) {
+	 Category category = Category.builder().name(categoryName).build();
+	 categoryRepository.save(category);
+	 
+	}
 
 }
