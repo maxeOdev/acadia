be03688c3d7ca0e62b6fe43a2824e23ee67f5a24
alalami63 <alalami@humanbooster.com>
2019-02-20 09:43:20 +0100

    Implementation of Training's repository

--- src/main/java/com/hb/acadia/repository/TrainingRepository.java
+++ src/main/java/com/hb/acadia/repository/TrainingRepository.java
@@ -1,9 +1,36 @@
 package com.hb.acadia.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.hb.acadia.model.Category;
 import com.hb.acadia.model.Training;
+import com.hb.acadia.model.user.Trainer;
 
+/**
+ * 
+ * @author anis
+ *
+ */
 public interface TrainingRepository extends JpaRepository<Training, Long> {
 
+	/**
+	 * @param id
+	 * @return Corresponding Training
+	 */
+	Training findById(long id);
+	
+	/**
+	 * @param category
+	 * @return Corresponding Trainings
+	 */
+	Set<Training> findByCategory(Category category);
+	
+	/**
+	 * @param trainer
+	 * @return Corresponding Trainings
+	 */
+	Set<Training> findByTrainer(Trainer trainer);
+	
 }
