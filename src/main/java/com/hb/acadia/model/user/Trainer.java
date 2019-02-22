bcf7c1bd3f47b9e9f6ad6cd89b0e364b8493830d
Aliotti <sim.aliotti@gmail.com>
2019-02-22 16:29:57 +0100

    update Trainer pojo

--- src/main/java/com/hb/acadia/model/user/Trainer.java
+++ src/main/java/com/hb/acadia/model/user/Trainer.java
@@ -65,10 +65,8 @@ public class Trainer extends User {
 		final int prime = 31;
 		int result = super.hashCode();
 		result = prime * result + experience;
-		result = prime * result + (int) (idStripe ^ (idStripe >>> 32));
 		result = prime * result + (isCertified ? 1231 : 1237);
 		result = prime * result + ((qualifications == null) ? 0 : qualifications.hashCode());
-		result = prime * result + ((trainings == null) ? 0 : trainings.hashCode());
 		return result;
 	}
 
@@ -80,13 +78,8 @@ public class Trainer extends User {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User u = (User) obj;
-		if (!u.equals(obj))
-			return false;
 		Trainer other = (Trainer) obj;
 		if (experience != other.experience)
-			return false;
-		if (idStripe != other.idStripe)
 			return false;
 		if (isCertified != other.isCertified)
 			return false;
@@ -95,12 +88,12 @@ public class Trainer extends User {
 				return false;
 		} else if (!qualifications.equals(other.qualifications))
 			return false;
-		if (trainings == null) {
-			if (other.trainings != null)
-				return false;
-		} else if (!trainings.equals(other.trainings))
-			return false;
 		return true;
 	}
+
+
+	
+
+	
 	
 }
