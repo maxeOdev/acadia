911361ecd32a1255b664f4badfff3c4f99514a97
Aliotti <sim.aliotti@gmail.com>
2019-03-12 14:50:48 +0100

    Add messag error for login page

--- src/main/java/com/hb/acadia/controller/LoginController.java
+++ src/main/java/com/hb/acadia/controller/LoginController.java
@@ -1,16 +1,29 @@
 package com.hb.acadia.controller;
 
+import com.hb.acadia.model.user.User;
+import com.hb.acadia.service.UserService;
+import com.hb.acadia.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value ="/login", method = RequestMethod.GET)
     public ModelAndView login(){
         return new ModelAndView("login");
     }
 
-}
+
+    }
+
+
