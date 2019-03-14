60560f05cdeb59006995b166fd05f603697234d4
alalami63 <alalami@humanbooster.com>
2019-03-14 12:16:35 +0100

    Implement training controller.

--- src/main/java/com/hb/acadia/controller/TrainingController.java
+++ src/main/java/com/hb/acadia/controller/TrainingController.java
@@ -1,11 +1,16 @@
 package com.hb.acadia.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hb.acadia.model.Training;
 import com.hb.acadia.service.TrainingService;
 
 @Controller
@@ -14,12 +19,14 @@ public class TrainingController {
 
 	@Autowired
 	private TrainingService trainingService;
-	
+
 	@GetMapping("/trainings")
 	public ModelAndView trainings() {
 		ModelAndView mav = new ModelAndView("trainings");
-		mav.addObject("trainings", trainingService.getAllTrainings());
+		Map<String, List<Training>> data = new HashMap<>();
+		data.put("trainings", trainingService.getAllTrainings());
+		mav.addObject("data", data);
 		return mav;
 	}
-	
+
 }
