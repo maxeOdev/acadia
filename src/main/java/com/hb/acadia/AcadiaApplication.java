d01680ade565b0d227760b628d53e78c28e99fdd
alalami63 <alalami@humanbooster.com>
2019-03-14 12:21:57 +0100

    Set local configuration for development.

--- src/main/java/com/hb/acadia/AcadiaApplication.java
+++ src/main/java/com/hb/acadia/AcadiaApplication.java
@@ -1,17 +1,18 @@
 package com.hb.acadia;
 
-import com.hb.acadia.model.Address;
-import com.hb.acadia.model.user.Role;
-import com.hb.acadia.model.user.User;
-import com.hb.acadia.repository.RoleRepository;
-import com.hb.acadia.repository.UserRepository;
-import com.hb.acadia.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+
+import com.hb.acadia.model.user.Role;
+import com.hb.acadia.repository.RoleRepository;
+import com.hb.acadia.service.CategoryService;
+import com.hb.acadia.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 
@@ -23,6 +24,11 @@ public class AcadiaApplication {
 	private RoleRepository roleRepository;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private CategoryService categoryService;
+
+	@Value("${spring.profiles.active}")
+	private String profile;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcadiaApplication.class, args);
@@ -31,33 +37,42 @@ public class AcadiaApplication {
 	//save Roles in db
 	@EventListener(ApplicationReadyEvent.class)
 	public void saveRolesAfterStartup() {
-		Role customerRole = new Role();
-		customerRole.setRoleName("ROLE_CUSTOMER");
-		Role adminRole = new Role();
-		adminRole.setRoleName("ROLE_ADMIN");
-		roleRepository.save(customerRole);
-		roleRepository.save(adminRole);
-		log.info("");
-		log.info("****** Roles saved in DB ******");
-		log.info("");
 
-		Address address = new Address();
-		address.setCity("Cailloux-sur-Fontaine");
-		address.setCountry("France");
-		address.setNumber(34);
-		address.setRoad("Du chateau");
-		address.setCp("35876");
-		address.setRoadType("Avenue");
-		
-		User simone = new User();
-		simone.setEmail("sim.aliotti@gmail.com");
-		simone.setFirstName("simon");
-		simone.setName("Aliotti");
-		simone.setPassword("toto");
-		simone.setActif(true);
-		simone.setAddress(address);
-		userService.createUser(simone);
+		if (profile.equals("local")) {
 
+			Role customerRole = new Role();
+			customerRole.setRoleName("ROLE_CUSTOMER");
+			Role adminRole = new Role();
+			adminRole.setRoleName("ROLE_ADMIN");
+			roleRepository.save(customerRole);
+			roleRepository.save(adminRole);
+			log.info("");
+			log.info("****** Roles saved in DB ******");
+			log.info("");
+
+			//		Address address = new Address();
+			//		address.setCity("Cailloux-sur-Fontaine");
+			//		address.setCountry("France");
+			//		address.setNumber(34);
+			//		address.setRoad("Du chateau");
+			//		address.setCp("35876");
+			//		address.setRoadType("Avenue");
+			//
+			//
+			//
+			//		User simone = new User();
+			//		simone.setEmail("sim.aliotti@gmail.com");
+			//		simone.setFirstName("simon");
+			//		simone.setName("Aliotti");
+			//		simone.setPassword("toto");
+			//		simone.setActif(true);
+			//		simone.setAddress(address);
+			//		userService.createUser(simone);
+
+			categoryService.createCategory("informatique");
+			categoryService.createCategory("bricolage");
+			categoryService.createCategory("gestion");
+		}
 	}
 }
 
