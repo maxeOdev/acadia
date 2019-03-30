6a9d66dacbecfa8569923e86b52c54af50f3399c
alalami63 <alalami@humanbooster.com>
2019-03-30 14:32:45 +0100

    Complete constraints and error messages for model Video and Training.

--- src/main/java/com/hb/acadia/model/Training.java
+++ src/main/java/com/hb/acadia/model/Training.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import com.hb.acadia.model.user.Trainer;
@@ -32,35 +33,44 @@ public class Training {
 	@NotNull
 	private String uuid;
 	
-	@NotNull
+	@NotNull(message = "Aucun titre renseigné.")
+	@NotEmpty(message = "Aucun titre renseigné.")
+	private String title;
+	
+	@NotNull(message = "Aucune description renseignée.")
+	@NotEmpty(message = "Aucune description renseignée.")
+	private String description;
+	
+	@NotNull(message = "Aucune difficulté renseignée.")
+	@NotEmpty(message = "Aucune difficulté renseignée.")
+	private Level difficulty;
+	
+	@NotNull(message = "Aucune durée renseignée.")
+	@NotEmpty(message = "Aucune durée renseignée.")
+	private int duration;
+	
+	@NotNull(message = "Aucun prix renseigné.")
+	@NotEmpty(message = "Aucun prix renseigné.")
+	private double price;
+	
+	private boolean isActive;
+	
+	@NotNull(message = "Aucun formateur associé.")
+	@NotEmpty(message = "Aucun formateur associé.")
 	@ManyToOne(fetch = FetchType.EAGER)
 	private Trainer trainer;
 	
-	@NotNull
+	@NotEmpty(message = "Aucune catégorie associé.")
 	@ManyToOne(fetch = FetchType.EAGER)
 	private Category category;
 	
-	@NotNull
+	@NotNull(message = "Aucune vidéo associée.")
+	@NotEmpty(message = "Aucune vidéo associée.")
 	@ManyToMany(fetch=FetchType.EAGER)
 	private Set<Video> videos;
 	
 	@OneToMany(mappedBy="training",fetch=FetchType.EAGER)
 	private Set<Comment> comments;
-	
-	@NotNull
-	private String description;
-	
-	@NotNull
-	private Level difficulty;
-	
-	@NotNull
-	private int duration;
-	
-	@NotNull
-	private double price;
-	
-	private boolean isActive;
-
 	
 	/**
 	 * 
@@ -88,10 +98,16 @@ public class Training {
 	 * @param price
 	 * @param isActive
 	 */
-	public Training(@NotNull Trainer trainer, @NotNull Category category, @NotNull Set<Video> videos,
-			Set<Comment> comments, @NotNull String description, @NotNull Level difficulty, @NotNull int duration,
-			@NotNull double price, boolean isActive) {
-		this();
+	public Training(@NotNull @NotEmpty String title, 
+			@NotNull @NotEmpty Trainer trainer, 
+			@NotNull @NotEmpty Category category,
+			@NotNull @NotEmpty Set<Video> videos, 
+			Set<Comment> comments, 
+			@NotNull @NotEmpty String description, 
+			@NotNull @NotEmpty Level difficulty,
+			@NotNull @NotEmpty int duration, 
+			@NotNull @NotEmpty double price, boolean isActive) {
+		this.title = title;
 		this.trainer = trainer;
 		this.category = category;
 		this.videos = videos;
