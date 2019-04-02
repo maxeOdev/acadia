2515e89c83cb48eb242e4fdde3e15171a98ae5ce
Aliotti <sim.aliotti@gmail.com>
2019-04-03 01:08:22 +0200

    Working on loading modal
    -modal start but doesn't stop -> have to fix it

--- src/main/resources/static/javascript/trainers.js
+++ src/main/resources/static/javascript/trainers.js
@@ -1,34 +1,50 @@
-
-$(document).ready(function () {
+/**
+ * Start just after loading html
+ */
+$(document).ready(function() {
 loadTrainers();
-})
+});
 
-$(document).on('click','.list-category', loadTrainers)
+//On click events
+$(document).on('click','.list-trainer', function(){
+loadTrainers();
+});
 
 
 
+
+
+/**
+ * Load trainers
+ */
 function loadTrainers() {
 $('#content').load('/admin/trainers-template-list',
 
+
 function (){
+//console.log($('#modal-loading'));
+    $('#modal-loading').modal('show');
     $.ajax({
         url: "/admin/trainers-list",
         type: "GET",
         data: {pageNumber: 0},
         success: function(trainersPage) {
-        let trainers = trainersPage['content'];
-        //let trainers = trainersPage.content;
+            let trainers = trainersPage['content'];
+            //let trainers = trainersPage.content;
             $('#content').html(handleBarsGenerateTrainersList(trainers));
             console.log(trainers.length + ' trainers loaded');
-        },
-        error: function(){
-                 console.log('error') ;
-               }
+            },
+            error: function(){
+            console.log('error');
+            }
     });
-  })
+$('#modal-loading').modal('show');
+  });
 }
 
-
+/**
+ * Generate HandleBars template => list trainers
+ */
 function handleBarsGenerateTrainersList(trainers) {
     let source = $('#handlebars-list-template').html();
     let template = Handlebars.compile(source);
