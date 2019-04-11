ab84f52af6fe9bfaea6af2914c5baa6b3b1f1946
Simon Aliotti <sim.aliotti@gmail.com>
2019-04-11 20:39:07 +0200

    Set properties in application.properties
    - And add error message in auth controller for signin method

--- acadia-rest-api/src/main/java/com/hb/controller/AuthController.java
+++ acadia-rest-api/src/main/java/com/hb/controller/AuthController.java
@@ -4,11 +4,14 @@ package com.hb.controller;
 import com.hb.Payload.JwtAuthenticationResponse;
 import com.hb.Payload.LoginRequest;
 import com.hb.Security.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +24,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/auth")
 @Validated
+@Slf4j
 public class AuthController {
 
     @Autowired
@@ -29,16 +33,24 @@ public class AuthController {
     @Autowired
     JwtTokenProvider jwtTokenProvider;
 
-    @RequestMapping(value="/signin", method = RequestMethod.POST)
-    public ResponseEntity<?> signin(@Valid @RequestBody LoginRequest loginRequest){
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public ResponseEntity<?> signin(@Valid @RequestBody LoginRequest loginRequest) {
 
-        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken
-                (loginRequest.getEmail(), loginRequest.getPassword()));
+        Authentication authenticate = null;
+
+        try {
+            authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken
+                    (loginRequest.getEmail(), loginRequest.getPassword()));
+        } catch (AuthenticationException e) {
+            log.info("***********************************************");
+            log.info("Authentication error : " + e);
+            log.info("***********************************************");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erreur lors de l'authentification, login/password inconnu(s)");
+
+        }
 
         SecurityContextHolder.getContext().setAuthentication(authenticate);
-
         String jwt = jwtTokenProvider.generateToken(authenticate);
-
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
     }
 
